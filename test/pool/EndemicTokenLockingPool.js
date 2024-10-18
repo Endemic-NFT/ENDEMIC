8ef6b31ccb560f1d2317852ac317735085f64f59
mradmilovic <marin.radmilovic@outlook.com>
2024-10-18 09:44:06 +0200

    Add tests for updating fee reciever

--- test/pool/EndemicTokenLockingPool.js
+++ test/pool/EndemicTokenLockingPool.js
@@ -5,10 +5,10 @@ const { fastForwardTime } = require('../helpers/time');
 const { Currencies, TimePeriods, Errors } = require('./constants');
 
 describe('EndemicTokenLockingPool', function () {
-  let owner, addr1, endemicToken, endemicTokenLockingPool, snapshotId;
+  let owner, addr1, addr2, endemicToken, endemicTokenLockingPool, snapshotId;
 
   before(async function () {
-    [owner, addr1] = await ethers.getSigners();
+    [owner, addr1, addr2] = await ethers.getSigners();
 
     [endemicToken, endemicTokenLockingPool] =
       await deployEndemicTokenPoolWithDeps(owner);
@@ -93,5 +93,57 @@ describe('EndemicTokenLockingPool', function () {
       endemicTokenLockingPool,
       Errors.InsufficientAmount
     );
+  });
+
+  describe('Fee Receiver Update', function () {
+    it('Should update the fee receiver and send fees to the new address', async function () {
+      // Initial setup: approve and lock tokens
+      await endemicToken
+        .connect(addr1)
+        .approve(endemicTokenLockingPool.address, Currencies.TWO_ETHER);
+
+      await endemicTokenLockingPool
+        .connect(addr1)
+        .liquidLock(Currencies.ONE_ETHER);
+
+      // Withdraw and check initial fee receiver balance
+      const initialFeeReceiverBalance = await endemicToken.balanceOf(
+        owner.address
+      );
+      await endemicTokenLockingPool
+        .connect(addr1)
+        .withdrawLiquidLockImmediately();
+
+      const fee = ethers.utils.parseEther('0.1'); // 10% fee
+      const newFeeReceiverBalance = await endemicToken.balanceOf(owner.address);
+      expect(newFeeReceiverBalance.sub(initialFeeReceiverBalance)).to.equal(
+        fee
+      );
+
+      // Update fee receiver to addr1
+      await endemicTokenLockingPool
+        .connect(owner)
+        .updateFeeReceiver(addr2.address);
+
+      // Lock and withdraw again
+      await endemicTokenLockingPool
+        .connect(addr1)
+        .liquidLock(Currencies.ONE_ETHER);
+
+      const initialAddr2Balance = await endemicToken.balanceOf(addr2.address);
+      await endemicTokenLockingPool
+        .connect(addr1)
+        .withdrawLiquidLockImmediately();
+
+      const newAddr2Balance = await endemicToken.balanceOf(addr2.address);
+      expect(newAddr2Balance.sub(initialAddr2Balance)).to.equal(fee);
+    });
+
+    it('Should fail to update the fee receiver if not called by the owner', async function () {
+      // Attempt to update fee receiver from a non-owner account
+      await expect(
+        endemicTokenLockingPool.connect(addr1).updateFeeReceiver(addr2.address)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
   });
 });
